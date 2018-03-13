244fa858f090ee28ab1356394705a1d1b19f46e2
tunime <tunime.1@gmail.com>
2018-03-12 19:59:15 -0500

    alistando el servedor y montandolo

---
+++ server/main.js
@@ -0,0 +1,23 @@
+//llama a la express
+var express = require('express');
+var app = express();
+//la variable que requiere el http
+var server = require('http').Server(app);
+//la variable io tendra todas la funcionalidades de los socket.io
+var io = require('socket.io')(server);
+var messages = [{
+	author: "Carlos",
+    text: "Hola! que tal?"
+},{
+	author: "Pepe",
+    text: "Muy bien! y tu??"
+},{
+	author: "Paco",
+    text: "Genial!"
+}];
+// el servidor corriendo
+//node server/main.js
+server.listen(8080, function() {
+	console.log('Servidor corriendo en http://localhost:8080');
+});
+
